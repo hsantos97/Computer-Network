d996f95af34d0d415456ebe692e6544cea921171
Harison Santos <harison_vr@hotmail.com>
2022-02-16 20:06:38 -0300

    work2 att 2

--- scanPort.py
+++ scanPort.py
@@ -2,7 +2,7 @@ import socket
 import sys
 
 if len(sys.argv) < 2:
-    print('Escolha um parametro ex: {} www.youtube.com'.format(sys.argv[0]))
+    print('Escolha um parametro ex: python3 {} www.youtube.com'.format(sys.argv[0]))
     exit()
 
 target = sys.argv[1]
@@ -10,22 +10,24 @@ print('Escaneando host com ip {}'.format(target))
 
 abertas  = 0
 fechadas = 0
-
-for porta in range(1, 7000):
+   
+for porta in range(1, 65535): 
     c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        c.settimeout(10)
-        r = c.connect_ex((target, porta))
+    c.settimeout(5)  
+    r = c.connect_ex((target, porta))
 
-        if r == 0:
+    if r == 0:
+        try: #para portas que o getservbyport não encontra ex: porta 7070 - realserver
             print(f'Porta {porta} Aberta. {socket.getservbyport(porta)}'+'\n')
             abertas += 1
-        else:
-            #print(f'Porta {porta} Fechada.'+'\n')
-            fechadas += 1
-    except socket.timeout:
-        print('Tempo esgotado !')
-
-print('Portas abertas {} -- Portas Fecahdas {}'.format(abertas, fechadas))
+        except:
+            pass
+    elif r == 11:
+        print('TEMPO ESGOTADO PARA PORTA', porta)
+    else:
+        #print(f'Porta {porta} Fechada.'+'\n')
+        fechadas += 1
 
 c.close()
+
+print('Portas abertas {} -- Portas Fechadas {}'.format(abertas, fechadas))
